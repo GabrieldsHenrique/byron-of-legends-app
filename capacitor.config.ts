3a5561aefad06aa6a796812ca37df0f1deebbbf4
Gabriel Henrique <gabriel.henrique@globalfinanceiro.com.br>
2024-04-29 10:42:01 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'byronoflegends-app',
+  webDir: 'www'
+};
+
+export default config;
